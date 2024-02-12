4959c8d8d8ff17ebfed7d7996966fe05ee0f9f02
kipkosgeicornelious <kipkosgeicornelious6@gmail.com>
2024-02-12 22:52:46 +0300

    done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import  "./App.css";
+import BotsPage from "./components/BotsPage"
+
+function App() {
+  return (
+    <div className="App">
+      <BotsPage />
+    </div>
+  );
+}
+
+export default App;
